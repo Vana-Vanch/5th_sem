a7cf4d33a3875cc9656980edb72c3ebc08369a27
Vana-Vanch <vanavanch98@gmail.com>
2021-07-23 15:19:34 +0530

    login vel tho

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,15 +18,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//home
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 //auth
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store'])->name('register');
+Route::post('/logout', [LogoutController::class, 'index'])->name('logout');
 
-Route::get('/dashboard', function(){
-    return view('pages.home');
-});
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
